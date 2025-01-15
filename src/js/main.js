2f7d651b110101af0e2a3dad1de3868c004d41e0
Tania Salvatella <tsalvatellap@gmail.com>
2025-01-15 18:42:28 +0100

    style

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 'use strict';
-
 let series = [];
 let favoritSeries = [];
 
@@ -8,21 +7,53 @@ const btnSearch = document.querySelector ('.js-btnSearch');
 const inputSearch = document.querySelector ('.js-search');
 const ulFavorites = document.querySelector ('.js-favorites');
 
+const btnResetFavorit = document.querySelector ('.js-btnFavorite');
+const btnReset = document.querySelector ('.js-btnReset');
+
+//BONUS
+//Valores que hay que resetear
+function resetValues (){
+  ulSearch.innerHTML = "";
+ 
+}
+
+function resetFavoritValue (){
+  ulFavorites.innerHTML ="";
+  localStorage.removeItem('favoritesServer');
+}
+
+//funcion para escuchar el evento reset y ejecutar funciones de reseteo
+function handleClickReset(ev){
+  ev.preventDefault();
+  resetValues ();
+  resetFavoritValue ();
+}
+
+btnReset.addEventListener('click', handleClickReset);
+
+//escuchar evento de boton reset favoritos
+//NO REFRESCA BIEN, CREO QUE TENGO MAL PUESTA LS
+function handleClickResetFavorit(ev){
+  ev.preventDefault();
+  resetFavoritValue ();
+}
+
+btnResetFavorit.addEventListener('click', handleClickResetFavorit);
+
 
 //funcion para pintar los favoritos
 function renderFavoritSeries (favorites){ 
   ulFavorites.innerHTML ="";
   for (const oneFav of favorites){
   ulFavorites.innerHTML += `
-  <li id="${oneFav.mal_id}">
-            <div class="favorites">
-              <button>X</button>
-              <img src="${oneFav.images.jpg.image_url}" alt="imagen de la serie">
+  <li id="${oneFav.mal_id}" class=" favorites" >
+            <div class="favorit_li" >
+              <button class="btnX" >&#215</button>
               <h3>${oneFav.title}</h3>
-            </div>
+              <img class ="favorit_img" src="${oneFav.images.jpg.image_url}" alt="imagen de la serie">
+             </div>
           </li>`;
   }
- 
 
 }
 
@@ -42,6 +73,7 @@ const indexFavoritSeries = favoritSeries.findIndex ((eachSerie)=> eachSerie.mal_
     //console.log (favoritSeries);
     renderFavoritSeries (favoritSeries);
      }
+  
   localStorage.setItem('favoritesServer', JSON.stringify(favoritSeries));  
     
   };
@@ -56,20 +88,38 @@ const listenerFavorit = ()=>{
 
 }
 
+// Cargar los datos de favoritos almacenado en el LS
+function loadFavorites(){
+const favoritSeriesLS = localStorage.getItem('favoritesServer');
+if (favoritSeriesLS){
+favoritSeries = JSON.parse (favoritSeriesLS);
+renderFavoritSeries (favoritSeries);
+}
+}
+
+
 //Funcion que pintar la lista de todas las series en la ul
 function renderSeries (list){
+  ulSearch.innerHTML = "";
     for (const serie of list){
+      // en mi array de favoritos voy a buscar si la serie ya está en favoritos
+    const findFav = favoritSeries.find((serieFav) => serieFav.id === serie.id);
+
+// NO SE POR QUE NO ME FUNCIONA, NO ME COJE LA CLASE
+    let css = findFav ? 'favorites' : '' ;
+    
     ulSearch.innerHTML += `
-    <li id="${serie.mal_id}" class="js-seriesLi">
-      <img src="${serie.images.jpg.image_url}" alt="imagen de la serie"/>
+    <li id="${serie.mal_id}" class="${css} js-seriesLi result_li ">
+      <img src="${serie.images.jpg.image_url}" 
+      alt="imagen de la serie" 
+      onerror="this.onerror=null; this.src='https://placehold.co/400x600';"/>
       <h3>${serie.title}</h3>
      
     </li>`;
 
     }
   listenerFavorit();
-
-}
+ }
   
 //Funcion para recoger los datos de la Api
 function getDataApi() {
@@ -81,6 +131,7 @@ function getDataApi() {
         series = info.data;
         //Se llama a la funcion que pinta los datos para que salgan una vez la api da los datos
         renderSeries (series);
+        loadFavorites();
         
        //console.log(series);
              
@@ -88,10 +139,7 @@ function getDataApi() {
   }
   getDataApi();
  
-  
-
-  //Buscador, funcion para escuchar el evento y filtrar por titulo las series que busque la usuaria
-
+    //Buscador, funcion para escuchar el evento y filtrar por titulo las series que busque la usuaria
   function handleSearch (event){
     event.preventDefault();
     ulSearch.innerHTML = "";
@@ -104,11 +152,3 @@ function getDataApi() {
 
   btnSearch.addEventListener("click", handleSearch);
 
-
- const favoritSeriesLS = localStorage.getItem('favoritesServer');
- 
- if (favoritSeriesLS){
-  favoritSeries = JSON.parse (favoritSeriesLS);
-  renderFavoritSeries (favoritSeries);
- }
- 
